cecc96bac3c0bb6cb743f1741b40ea9ed63bb892
TangXiaoDe <xiaodehappy@foxmail.com>
2019-08-28 19:04:11 +0800

    player: 下载按钮显示更新

--- ZFPlayer/CustomPlayerControlView.m
+++ ZFPlayer/CustomPlayerControlView.m
@@ -1136,8 +1136,7 @@ static const CGFloat ZFPlayerControlBarAutoFadeOutTimeInterval = 0.35f;
  是否有下载功能
  */
 - (void)zf_playerHasDownloadFunction:(BOOL)sender {
-    //self.downLoadBtn.hidden = !sender;
-    self.downLoadBtn.hidden = YES;
+    self.downLoadBtn.hidden = !sender;
 }
 
 /**
